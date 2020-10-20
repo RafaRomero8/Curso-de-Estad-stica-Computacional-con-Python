f0af13679add9891c6722f5f3836186bb345dee2
RafaRomero8 <RafaRomerox@gmail.com>
2020-10-20 18:22:11 -0500

    varianza y desviacion estandar

--- media.py
+++ media.py
@@ -1,13 +1,31 @@
 import random
+import math
 
 def media(X):
-    return sum(X)/ len(X)#len longitud de los valores
+    return sum(X) / len(X)
 
+
+def varianza(X):
+    mu = media(X)
+
+    acumulador = 0
+    for x in X:
+        acumulador += (x - mu)**2
+
+    return acumulador / len(X)
+
+
+def desviacion_estandar(X):
+    return math.sqrt(varianza(X))
 
 
 if __name__ == '__main__':
-    X = [random.randint(1,21) for i in range(20)]
+    X = [random.randint(9, 12) for i in range(20)]
     mu = media(X)
+    Var = varianza(X)
+    sigma = desviacion_estandar(X)
 
-    print(X)
-    print(mu)
+    print(f'Arreglo X: {X}')
+    print(f'Media = {mu}')
+    print(f'Varianza = {Var}')
+    print(f'Desviacion estandar = {sigma}')
